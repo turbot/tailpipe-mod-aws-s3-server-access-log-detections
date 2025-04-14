adcde715bf0cfb7bb8d9caeae5e6d4c5308fec54
Ved misra <47312748+misraved@users.noreply.github.com>
2025-04-15 01:04:07 +0530

    Uncomment documentation field in mod.pp

--- mod.pp
+++ mod.pp
@@ -3,7 +3,7 @@ mod "aws_s3_server_access_log_detections" {
   title         = "AWS S3 Server Access Log Detections"
   description   = "Run detections and view dashboards for your AWS S3 server access logs to monitor and analyze activity across your S3 buckets using Powerpipe and Tailpipe."
   color         = "#FF9900"
-  # documentation = file("./docs/index.md")
+  documentation = file("./docs/index.md")
   icon          = "/images/mods/turbot/aws-s3-server-access-log-detections.svg"
   categories    = ["aws", "dashboard", "detections", "public cloud"]
   database      = var.database
